2604370d46a65bc4ab9241eecb11391c8f695514
Piper2818 <136010132+Piper2818@users.noreply.github.com>
2023-09-25 10:20:51 -0600

    Create GraduationCap.py

---
+++ GraduationCap.py
@@ -0,0 +1,8 @@
+while true: 
+  basic.show_string("Hello Real World")
+  basic.show_icon(IconNames.HEART)
+  basic.show_icon(IconNames.SMALL_HEART)
+  basic.show_icon(IconNames.HEART)
+  basic.show_icon(IconNames.SMALL_HEART)
+  basic.clear_screen()
+  
