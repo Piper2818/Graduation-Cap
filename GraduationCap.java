800369fc387d47c573c7a2c8f226f51ad04da8c9
Piper2818 <136010132+Piper2818@users.noreply.github.com>
2023-09-25 10:23:48 -0600

    Create GraduationCap.java

---
+++ GraduationCap.java
@@ -0,0 +1,8 @@
+while(true)
+{ basic.showString("Hello Real World") 
+  basic.showIcon(IconNames.Heart)
+  basic.showIcon(IconNames.SmallHeart)
+  basic.showIcon(IconNames.Heart)
+  basic.showIcon(IconNames.SmallHeart)
+  basic.clearScreen()
+  }
